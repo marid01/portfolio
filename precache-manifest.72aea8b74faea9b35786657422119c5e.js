e1d2ecfff4eeea227b222f27c125593a408cbd7f
Marid <maridshikhkerimov@gmail.com>
2022-03-16 22:40:38 +0300

    Updates

---
+++ precache-manifest.72aea8b74faea9b35786657422119c5e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761b4ef8580723d54695e7435ccd6c81",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6a89317064d3c5f48fb5",
+    "url": "/portfolio/static/css/main.013754e3.chunk.css"
+  },
+  {
+    "revision": "ec4eed1a42b3138f0f44",
+    "url": "/portfolio/static/js/2.70c96cbf.chunk.js"
+  },
+  {
+    "revision": "8414c91afd5a27f9ba9e7951b9346998",
+    "url": "/portfolio/static/js/2.70c96cbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a89317064d3c5f48fb5",
+    "url": "/portfolio/static/js/main.a9293b53.chunk.js"
+  },
+  {
+    "revision": "4c76482b968eb9f47af6",
+    "url": "/portfolio/static/js/runtime-main.98fb296d.js"
+  },
+  {
+    "revision": "f56e632818c626ed6244006f54caa492",
+    "url": "/portfolio/static/media/codewars_logo_icon_145389.f56e6328.svg"
+  },
+  {
+    "revision": "e4a57767b3fa6fc2bc617b3afc381a49",
+    "url": "/portfolio/static/media/giris.e4a57767.jpeg"
+  },
+  {
+    "revision": "fcdfcbc5ef155bf5a438d8cb75496525",
+    "url": "/portfolio/static/media/github_icon_128848.fcdfcbc5.svg"
+  },
+  {
+    "revision": "e4356db9fc1f88fdc1160909ea1bd9ec",
+    "url": "/portfolio/static/media/graphql.e4356db9.svg"
+  },
+  {
+    "revision": "f6967c81822f43ac6ae2fbd1b915c17e",
+    "url": "/portfolio/static/media/icons8-javascript.f6967c81.svg"
+  },
+  {
+    "revision": "d4a8ef063a99a8764cd0e1f793785b8c",
+    "url": "/portfolio/static/media/icons8-material-ui.d4a8ef06.svg"
+  },
+  {
+    "revision": "0811c32d2b6d01d24ab54ba6443dcb71",
+    "url": "/portfolio/static/media/icons8-nodejs.0811c32d.svg"
+  },
+  {
+    "revision": "3b599a6897e54a67af7cddaace22d7c8",
+    "url": "/portfolio/static/media/icons8-react.3b599a68.svg"
+  },
+  {
+    "revision": "a248a9913b70873c61db8720301a651e",
+    "url": "/portfolio/static/media/icons8-redux.a248a991.svg"
+  },
+  {
+    "revision": "265f332738c918914d039db95271af46",
+    "url": "/portfolio/static/media/icons8-typescript.265f3327.svg"
+  },
+  {
+    "revision": "9f330bf684706b5ae015ceb72be2409e",
+    "url": "/portfolio/static/media/laptop-5842509.9f330bf6.jpg"
+  },
+  {
+    "revision": "a869cddd31b6f679890698f75234cf7b",
+    "url": "/portfolio/static/media/mongodb.a869cddd.svg"
+  },
+  {
+    "revision": "06af58037cd5b3c344e32456fd9b42ef",
+    "url": "/portfolio/static/media/story.06af5803.svg"
+  },
+  {
+    "revision": "87f6917321cf68ff729e7336e50e7ca1",
+    "url": "/portfolio/static/media/telegram_icon-icons.com_53603.87f69173.svg"
+  }
+]);
